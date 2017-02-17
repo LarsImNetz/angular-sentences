023b0d9684e30081b2d350958d11c7a0f387ae57
Lars Langhans <lars.langhans@gmx.de>
2017-02-17 22:15:54 +0100

    change urls

--- gulp/config.js
+++ gulp/config.js
@@ -4,16 +4,16 @@
 
 module.exports.constants = {
     home: {
-        restApiRootUrl: "//localhost:8008/sentences-rest-server/satz/"
+        restApiRootUrl: "http://localhost:8008/sentences-rest-server/satz/"
     },
     moon: {
         restApiRootUrl: "http://moonserver.homenet.org:8080/sentencesrestbeanserver/satz/"
     },
     local: {
-        restApiRootUrl: "//localhost:8080/sentences-rest-server/satz/"
+        restApiRootUrl: "http://localhost:8080/sentences-rest-server/satz/"
     },
     qa: {
-        restApiRootUrl: "//qa.relaunch.drklein.hypoport.local/rest-api/tav/"
+        restApiRootUrl: "http://qa.relaunch.drklein.hypoport.local/rest-api/tav/"
     },
     production: {
         restApiRootUrl: "tbd."
--- test.sh
+++ test.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 HOSTNAME=$(hostname)
+HOSTTOTEST=${1:-$HOSTNAME}
 
-if [ "$HOSTNAME" == "pandemonium" ]; then
+if [ "$HOSTTOTEST" == "pandemonium" ]; then
     curl http://localhost:8008/sentences-rest-server/satz/test
     FAILURE=$?
     echo "FAILURE: $FAILURE"
-    # elif [ "$HOSTNAME" == "w7e186" ]; then
+elif [ "$HOSTTOTEST" == "moon" ]; then
+    curl http://moonserver.homenet.org:8080/sentencesrestbeanserver/satz/test
 fi
